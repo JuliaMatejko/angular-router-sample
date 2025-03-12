e5ca51ad8c62f5ee037d92b38e83e23016f44de5
Julia Matejko <matejkojulia@gmail.com>
2025-03-12 22:10:00 +0100

    Add profile component and a custom route matcher

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlSegment } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path: 'crisis-list', component: CrisisListComponent },
   { path: 'heroes-list', component: HeroesListComponent },
   { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+  {
+    matcher: (url) => {
+      if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+        return {consumed: url, posParams: {username: new UrlSegment(url[0].path.slice(1), {})}};
+      }
+      return null;
+    },
+    component: ProfileComponent,
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
